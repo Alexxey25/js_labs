047f650f349f6d993c7cf21d45a60d243ef40f39
Alexxey25 <alexalexandrov2005@gmail.com>
2025-05-05 10:43:10 +0300

    Лаба 6 с boundle без Header

--- lab4/example-nestjs/src/app.module.ts
+++ lab4/example-nestjs/src/app.module.ts
@@ -2,9 +2,18 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { VehiclesModule } from './vehicles/vehicles.module';
+import { ServeStaticModule } from '@nestjs/serve-static';
+import { join } from 'path';
 
 @Module({
-  imports: [VehiclesModule],
+  imports: [
+    ServeStaticModule.forRoot({
+      rootPath: join(__dirname, '..', 'client'), // Путь к папке с фронтендом
+      serveRoot: '/', // Отдавать файлы по корневому URL
+      exclude: ['/api*'], // Важно: исключить API маршруты
+    }),
+    VehiclesModule
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
--- lab4/example-nestjs/src/main.ts
+++ lab4/example-nestjs/src/main.ts
@@ -1,8 +1,18 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { CorsOptions } from '@nestjs/common/interfaces/external/cors-options.interface';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule, { cors: true} );
+  const app = await NestFactory.create(AppModule);
+  const corsOptions: CorsOptions = {
+    origin: '*',
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS', 'PATCH'],
+    allowedHeaders: ['Content-Type', 'Authorization', 'ssctrl', 'll'],
+    credentials: true,
+  };
+
+  app.enableCors(corsOptions); 
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
