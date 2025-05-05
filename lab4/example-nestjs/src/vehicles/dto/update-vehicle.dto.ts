3e27edf17cdb08df5473f0bb2d3379cd3878996c
Alexxey25 <alexalexandrov2005@gmail.com>
2025-05-05 11:24:21 +0300

    Лаба 6

--- lab4/example-nestjs/src/vehicles/dto/update-vehicle.dto.ts
+++
@@ -1,5 +0,0 @@
-import { PartialType } from '@nestjs/mapped-types';
-import {CreateVehicleDto } from './create-vehicle.dto';
-
-export class UpdateVehicleDto extends PartialType(CreateVehicleDto) {}
-//все поля необязательные, удобно при обновлении карточки
